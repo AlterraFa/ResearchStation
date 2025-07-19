f3435aab175f38aaa9e7b16d3a8e150f927f31eb
AlterraFa <ntony8124@gmail.com>
2025-07-19 17:22:46 +0700

    [CHANGED]: Added multiple pointcloud.bin loader

--- ComputerVision/utils_3d/loader.py
+++ ComputerVision/utils_3d/loader.py
@@ -1,4 +1,7 @@
 import numpy as np
+import glob
+
+from tqdm.auto import tqdm
 
 def load_bin(pcPath: str) -> np.ndarray:
     data = np.fromfile(pcPath, dtype = np.float32)
@@ -36,3 +39,14 @@ def load_calib(calibPath: str) -> np.ndarray:
 def save_bin(pc: np.ndarray, filePath: str) -> None:
     assert pc.ndim == 2 and pc.shape[1] == 4, "pc must be N×4"
     pc.astype(np.float32).tofile(filePath)
+
+def load_multi_pc(load_folder: str, from_idx: int = 0, to_idx: int = -1) -> list[np.ndarray]:
+    file_paths = glob.glob(load_folder + "/*")
+    
+    assert to_idx < len(file_paths), f"List index out of range"
+    
+    pc_part = []
+    for path in tqdm(file_paths[from_idx: to_idx], desc = "Loading point clouds", unit = " Files"):
+        pc_part += [load_bin(path)]
+        
+    return pc_part
